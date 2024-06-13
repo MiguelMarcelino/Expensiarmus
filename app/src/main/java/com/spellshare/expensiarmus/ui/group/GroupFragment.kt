acc1694efb6fa2acb269031ae107edccbb777dda
Miguel Marcelino <mac.marcelino@outlook.pt>
2024-06-13 13:22:43 +0100

    feature: Add modern group list

--- app/src/main/java/com/spellshare/expensiarmus/ui/group/GroupFragment.kt
+++ app/src/main/java/com/spellshare/expensiarmus/ui/group/GroupFragment.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.spellshare.expensiarmus.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.spellshare.expensiarmus.data.Group
 import com.spellshare.expensiarmus.databinding.FragmentGroupBinding
 import com.spellshare.expensiarmus.dbconnector.GroupConnector
 
@@ -27,28 +24,18 @@ class GroupFragment : Fragment() {
         _binding = FragmentGroupBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val listView: ListView = binding.listView
 
         val groupConnector = GroupConnector()
         val groupItems = groupConnector.getItems()
-        val adapter = ArrayAdapter(
-            requireContext(),
-            android.R.layout.simple_list_item_1,
-            groupItems.map { it.name })
-        listView.adapter = adapter
-
-        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            val selectedItem = groupItems[position]
-            val bundle = Bundle().apply {
-                putString("uid", selectedItem.uid)
-                putString("name", selectedItem.name)
-                putString("description", selectedItem.description)
-                putString("ownerUid", selectedItem.ownerUid)
-            }
-            findNavController().navigate(R.id.action_nav_group_to_nav_group_detail, bundle)
-        }
+        displayGroups(groupItems)
 
         return root
+    }
+
+    private fun displayGroups(groups: List<Group>) {
+        val adapter = GroupAdapter(groups)
+        binding.groupRecyclerView.layoutManager = LinearLayoutManager(context)
+        binding.groupRecyclerView.adapter = adapter
     }
 
     override fun onDestroyView() {
