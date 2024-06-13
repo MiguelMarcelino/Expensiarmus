ec065e9ffc8a9700428962731da674c2f7f389e1
Miguel Marcelino <mac.marcelino@outlook.pt>
2024-06-13 15:12:37 +0100

    fix: Add back buttin

--- app/src/main/java/com/spellshare/expensiarmus/MainActivity.kt
+++ app/src/main/java/com/spellshare/expensiarmus/MainActivity.kt
@@ -3,12 +3,15 @@ package com.spellshare.expensiarmus
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.spellshare.expensiarmus.databinding.ActivityMainBinding
@@ -25,18 +28,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Set up the navigation controller
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
-
         if (navHostFragment == null) {
             Log.e("MainActivity", "NavHostFragment not found")
             return
         }
-
         val navController = navHostFragment.navController
 
+        // Set up the bottom navigation bar
         val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
         navView.setupWithNavController(navController)
+
+        // Set up the app bar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         // Initialize Firebase
         FirebaseApp.initializeApp(this)
@@ -48,8 +55,14 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()  // Handle the back button press
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
